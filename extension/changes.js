d329fd45f577c93c403a12222b6c5b5a2e81b649
Gavin Jamieson <gavinjamieson@qantas.com.au>
2020-01-22 16:40:00 +1100

    Added go button to barcode search

--- extension/changes.js
+++ extension/changes.js
@@ -1086,9 +1086,18 @@ function runLint() {
     }
 }
 
+function addGoButton() {
+    //Mark Rowans button
+    var gobutton = document.createElement("button");
+    gobutton.setAttribute("type", "submit");
+    gobutton.innerHTML = "GO";
+    document.getElementById("idHeaderSearchWrap").childNodes[1].appendChild(gobutton);
+}
+
 //This runs on every page load...  Its job is to determine the page title and the user and then enable the correct enhancements for that page.
 //It also grabs the users staff number.  Its a provision for user settings specific settings in the future.  The idea is a setting is stored on the server and can then follow the user to different computers
 function mainrun() {
+    addGoButton();
     betatesters = ["318955", 
                    "250384", 
                    "346915", 
